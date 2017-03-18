155190b92a36feff4d9feaa717310867b3e2fdb6
Gabriel C. Troia <catalin.troia@gmail.com>
2017-03-18 17:58:16 -0400

    First commit

--- Liaison/ViewController.swift
+++ Liaison/ViewController.swift
@@ -8,18 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    //MARK: Properties
+    @IBOutlet weak var wordTextField: UITextField!
+    @IBOutlet weak var wordLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        wordTextField.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //MARK: Actions
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        wordLabel.text = "Reset value"
+        wordTextField.placeholder = "hmm"
     }
-
+    
+    //MARK: Text Field
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        wordLabel.text = textField.text
+    }
 
 }
 
