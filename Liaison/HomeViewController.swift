85aa1fea133701fc2c3dfa72a7e14c718c3e9acc
Gabriel C. Troia <catalin.troia@gmail.com>
2017-03-26 22:54:35 -0400

    Able to Record, save in a list, and play them by clicking on an item

--- Liaison/HomeViewController.swift
+++ Liaison/HomeViewController.swift
@@ -7,11 +7,19 @@
 //
 
 import UIKit
+import AVFoundation
 
-class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, AVAudioRecorderDelegate, AVAudioPlayerDelegate {
     
     //MARK: Properties
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var recordButton: UIButton!
+    
+    var recordingSession: AVAudioSession!
+    var recorder: AVAudioRecorder!
+    var player: AVAudioPlayer!
+    
+    var audioURL: URL!
     
     var records = [Record]()
     
@@ -19,11 +27,108 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        loadSampleRecords()
+//        loadSampleRecords()
         
         tableView.register(RecordTableViewCell.self, forCellReuseIdentifier: "DefaultCell")
+        
+        recordButton.addTarget(self, action: #selector(toggleRecord),for: .touchDown)
+        
+        recordingSession = AVAudioSession.sharedInstance()
+        
+        do {
+            try recordingSession.setCategory(AVAudioSessionCategoryPlayAndRecord)
+            try recordingSession.setActive(true)
+            
+            recordingSession.requestRecordPermission({ [unowned self] allowed in
+                DispatchQueue.main.async {
+                    if !allowed {
+                        self.loadFailUI()
+                    }
+                }
+                
+            })
+        } catch {
+            loadFailUI()
+        }
     }
-
+    
+    func loadFailUI() {
+    }
+    
+    func toggleRecord() {
+        if recorder == nil {
+            startRecording()
+        } else {
+            finishRecording(success: true)
+        }
+    }
+    
+    func startRecording() {
+        print("recording")
+        
+        view.backgroundColor = UIColor.red
+        
+        recordButton.setTitle("Release", for: .normal)
+        recordButton.addTarget(self, action: #selector(toggleRecord), for: .touchUpInside)
+        
+        audioURL = getRecordingUrl();
+        print(audioURL.absoluteURL)
+        
+        let settings = [
+            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+            AVSampleRateKey: 12000,
+            AVNumberOfChannelsKey: 1,
+            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue,
+            ]
+        
+        do {
+            recorder = try AVAudioRecorder(url: audioURL, settings: settings)
+            recorder.delegate = self
+            recorder.record()
+        } catch {
+            finishRecording(success: false)
+        }
+    }
+    
+    func finishRecording(success: Bool) {
+        recorder.stop()
+        recorder = nil
+        
+        if success {
+            print("Recording finished successfully")
+            
+            saveNewRecord()
+            recordButton.setTitle("Hold", for: .normal)
+            
+        } else {
+            print("Recording failed")
+        }
+    }
+    
+    func saveNewRecord() {
+        guard let newRecord = Record(title: "new word \(records.count)", audioURL: audioURL) else {
+            fatalError("Unable to instantiate record")
+        }
+        
+        records += [newRecord];
+        tableView.reloadData()
+        
+        print("record \(newRecord.title) saved")
+        
+        
+    }
+    
+    func getRecordingUrl() -> URL {
+        let timestamp = Int64(NSDate().timeIntervalSince1970 * 1000)
+        return ViewController.getDocumentsDirectory().appendingPathComponent("recording-\(String(timestamp)).m4a")
+    }
+    
+    class func getDocumentsDirectory() -> URL {
+        return FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -52,21 +157,55 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
         
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("section: \(indexPath.section)")
+        print("row: \(indexPath.row)")
+        
+        let currentRecord = records[indexPath.row]
+        
+        startPlayer(audioURL: currentRecord.audioURL)
+    }
+    
+    func startPlayer(audioURL: URL) {
+        print("start player \(audioURL.absoluteURL)")
+        do {
+            
+            //playButton.setTitle("Stop", for: .normal)
+            
+            player = try AVAudioPlayer(contentsOf: audioURL)
+            player.delegate = self
+            
+            player!.prepareToPlay()
+            player!.play()
+        } catch {
+            print (error.localizedDescription)
+        }
+    }
+    
+    func stopPlayer() {
+        player!.stop()
+        player = nil
+        
+        //playButton.setTitle("Play", for: .normal)
+    }
+    
     private func loadSampleRecords() {
         guard let record1 = Record(title: "cat", audioURL: URL(string: "cat.m4a")!) else {
-            fatalError("Unable to instantiate meal1")
+            fatalError("Unable to instantiate record")
         }
         
         guard let record2 = Record(title: "dog", audioURL: URL(string: "cat.m4a")!) else {
-            fatalError("Unable to instantiate meal1")
+            fatalError("Unable to instantiate record")
         }
         
         guard let record3 = Record(title: "bird", audioURL: URL(string: "cat.m4a")!) else {
-            fatalError("Unable to instantiate meal1")
+            fatalError("Unable to instantiate record")
         }
         
         records += [record1, record2, record3]
     }
+    
+    
 
     /*
     // MARK: - Navigation
